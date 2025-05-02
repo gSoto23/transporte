3d1befbab189ca25e03a9eda2098a79a2a42b2f1
Gerardo <gerardo@gscodecr.com>
2025-05-02 17:31:50 -0600

    Adding new features

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     let precioPorKm;
     let montoMinimo;
+    const restOfForm = document.getElementById('restOfForm');
+    const vehicleImages = document.querySelectorAll('.vehicle-image');
+    const tipoVehiculoInput = document.getElementById('tipoVehiculo');
+    let costoAyudante = 15000; // Costo por ayudante
+
+    // Deshabilitar el formulario inicialmente
+    restOfForm.classList.remove('enabled');
 
     function actualizarPreciosPorVehiculo() {
-
         var fechaHoy = new Date().toISOString().substring(0, 10);
         document.getElementById('fechaEnvio').value = fechaHoy;
 
@@ -13,85 +19,19 @@ document.addEventListener('DOMContentLoaded', function () {
                 precioPorKm = 1500;
                 montoMinimo = 25000;
                 break;
-            case 'Camión 5 Ton +':
+            case 'Moto':
                 precioPorKm = 3000;
-                montoMinimo = 50000;
+                montoMinimo = 15000;
                 break;
-            case 'Plataforma 3 Ton':
-                precioPorKm = 2000;
+            case 'Mudanza':
+                precioPorKm = 5000;
                 montoMinimo = 30000;
-                break;    
-            case 'Plataforma 5 Ton +':
-                precioPorKm = 3000;
-                montoMinimo = 50000;
-                break;
-            case 'Vagoneta':
-                precioPorKm = 3000;
-                montoMinimo = 50000;
                 break;
             default:
-                precioPorKm = 3000;  // Precio por defecto para cualquier tipo no especificado
+                precioPorKm = 3000;
                 montoMinimo = 15000;
         }
     }
-    
-
-    document.getElementById('tipoVehiculo').addEventListener('change', function() {
-        actualizarPreciosPorVehiculo();
-        calcularDistanciaYPrecio();
-    });
-
-    // Cargar datos de provincias, cantones y distritos
-    fetch('/provincias-cantones-dsitritos.json')
-        .then(response => response.json())
-        .then(data => {
-            if (data && data.provincias) {
-                const provincias = Object.values(data.provincias);
-                const selectProvincia = document.getElementById('provincia');
-                const selectCanton = document.getElementById('canton');
-                const selectDistrito = document.getElementById('distrito');
-
-                provincias.forEach(provincia => {
-                    let option = document.createElement('option');
-                    option.value = provincia.nombre;
-                    option.text = provincia.nombre;
-                    selectProvincia.appendChild(option);
-                });
-
-                selectProvincia.addEventListener('change', function () {
-                    let provinciaSeleccionada = provincias.find(p => p.nombre === this.value);
-                    let cantones = Object.values(provinciaSeleccionada.cantones);
-                    selectCanton.innerHTML = '<option value="">Seleccione...</option>';
-                    selectDistrito.innerHTML = '<option value="">Seleccione...</option>';
-
-                    cantones.forEach(canton => {
-                        let option = document.createElement('option');
-                        option.value = canton.nombre;
-                        option.text = canton.nombre;
-                        selectCanton.appendChild(option);
-                    });
-                });
-
-                selectCanton.addEventListener('change', function () {
-                    let provinciaSeleccionada = provincias.find(p => p.nombre === selectProvincia.value);
-                    let cantonSeleccionado = Object.values(provinciaSeleccionada.cantones).find(c => c.nombre === this.value);
-                    let distritos = Object.values(cantonSeleccionado.distritos);
-                    selectDistrito.innerHTML = '<option value="">Seleccione...</option>';
-
-                    distritos.forEach(distrito => {
-                        let option = document.createElement('option');
-                        option.value = distrito;
-                        option.text = distrito;
-                        selectDistrito.appendChild(option);
-                    });
-                });
-            } else {
-                console.error('El formato del JSON no es válido o no contiene provincias.');
-            }
-        })
-        .catch(error => {
-            console.error('Error al cargar el JSON:', error);
-        });
 
     // Inicializar flatpickr para el campo de fecha
     flatpickr('#fechaEnvio', {
@@ -123,8 +63,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Mapa y marcadores para origen
     let mapOrigen = new google.maps.Map(document.getElementById('mapOrigen'), {
-        zoom: 8,
-        center: { lat: 9.748917, lng: -83.753428 } // Coordenadas de Costa Rica
+        zoom: 12,
+        center: { lat: 9.748917, lng: -83.753428 }
     });
 
     let markerOrigen = new google.maps.Marker({
@@ -141,8 +81,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Mapa y marcadores para destino
     let mapDestino = new google.maps.Map(document.getElementById('mapDestino'), {
-        zoom: 8,
-        center: { lat: 9.748917, lng: -83.753428 } // Coordenadas de Costa Rica
+        zoom: 12,
+        center: { lat: 9.748917, lng: -83.753428 }
     });
 
     let markerDestino = new google.maps.Marker({
@@ -151,13 +91,13 @@ document.addEventListener('DOMContentLoaded', function () {
         draggable: true
     });
 
-    // Eventos para actualizar al mover el marcador de destino
+    // Eventos para marcador de destino
     google.maps.event.addListener(markerDestino, 'dragend', function () {
         updateAddressInput(autocompleteDestino, document.getElementById('destino'), markerDestino.getPosition());
         calcularDistanciaYPrecio();
     });
 
-    // Actualizar campos al seleccionar una dirección del autocompletar
+    // Eventos de autocompletado
     autocompleteOrigen.addListener('place_changed', function () {
         let place = autocompleteOrigen.getPlace();
         if (place.geometry) {
@@ -176,7 +116,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Función para actualizar la dirección en el campo de texto
+    // Función para actualizar dirección
     function updateAddressInput(autocomplete, inputElement, position) {
         inputElement.value = '';
         if (position) {
@@ -189,7 +129,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // Calcular la distancia y el precio entre origen y destino
+    // Calcular distancia y precio
     function calcularDistanciaYPrecio() {
         actualizarPreciosPorVehiculo();
         let origen = markerOrigen.getPosition();
@@ -204,11 +144,15 @@ document.addEventListener('DOMContentLoaded', function () {
             }, function (response, status) {
                 if (status === 'OK') {
                     let distanceText = response.rows[0].elements[0].distance.text;
-                    let distanceValue = response.rows[0].elements[0].distance.value / 1000; // en km
+                    let distanceValue = response.rows[0].elements[0].distance.value / 1000;
                     let precio = Math.round(distanceValue * precioPorKm / 1000) * 1000;
-
-                    //validacion de precio minimo
                     precio = Math.max(precio, montoMinimo);
+
+                    const ayudanteSeleccionado = document.querySelector('input[name="ayudantes"]:checked');
+                    if (ayudanteSeleccionado) {
+                        const numeroAyudantes = parseInt(ayudanteSeleccionado.value);
+                        precio += (numeroAyudantes * costoAyudante);
+                    }
 
                     document.getElementById('distancia').value = distanceText;
                     document.getElementById('precio').value = precio.toLocaleString('es-CR', {
@@ -222,41 +166,33 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    function enviarCorreo(formData) {
-        emailjs.init('YOUR_EMAILJS_USER_ID');
-        emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', formData)
-            .then(function(response) {
-                console.log('Correo enviado con éxito!', response.status, response.text);
-            }, function(error) {
-                console.log('Error al enviar el correo.', error);
-            });
-    }
-    
-    function enviarWhatsApp(formData) {
-        const mensaje = `Tipo de Vehículo: ${formData.tipoVehiculo}\nFecha de Envío: ${formData.fechaEnvio}\nProvincia: ${formData.provincia}\nCantón: ${formData.canton}\nDistrito: ${formData.distrito}\nOrigen: ${formData.origen}\nDestino: ${formData.destino}\nDetalle: ${formData.detalle}\nNombre: ${formData.nombre}\nTeléfono: +506${formData.telefono}\nCorreo: ${formData.correo}\nDistancia: ${formData.distancia}\nPrecio: ${formData.precio}`;
-        const whatsappUrl = `https://api.whatsapp.com/send?phone=50670465000&text=${encodeURIComponent(mensaje)}`;
-        window.open(whatsappUrl, '_blank');
-    }
+    // Agregar eventos para los radio buttons de ayudantes
+    const radioAyudantes = document.querySelectorAll('input[name="ayudantes"]');
+    radioAyudantes.forEach(radio => {
+        radio.addEventListener('change', calcularDistanciaYPrecio);
+    });
 
-    // function enviarWhatsApp(formData) {
-    //     fetch('http://localhost:3000/send-message', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify({
-    //             message: `Tipo de Vehículo: ${formData.tipoVehiculo}\nFecha de Envío: ${formData.fechaEnvio}\nProvincia: ${formData.provincia}\nCantón: ${formData.canton}\nDistrito: ${formData.distrito}\nOrigen: ${formData.origen}\nDestino: ${formData.destino}\nDetalle: ${formData.detalle}\nNombre: ${formData.nombre}\nTeléfono: +506${formData.telefono}\nCorreo: ${formData.correo}\nDistancia: ${formData.distancia}\nPrecio: ${formData.precio}`,
-    //             // to: 'whatsapp-group-id',
-    //             to: `whatsapp:+50670465000`
-    //         })
-    //     })
-    //     .then(response => response.json())
-    //     .then(data => console.log('Respuesta del servidor:', data))
-    //     .catch(error => console.error('Error enviando mensaje:', error));
-    // }
-    
 
-    // Función para guardar los datos del formulario
+    // Manejo de selección de vehículos
+    vehicleImages.forEach(vehicle => {
+        vehicle.addEventListener('click', function() {
+            vehicleImages.forEach(v => v.classList.remove('selected'));
+            this.classList.add('selected');
+            tipoVehiculoInput.value = this.dataset.vehicle;
+            this.closest('.form-group').querySelector('.invalid-feedback').style.display = 'none';
+            restOfForm.classList.add('enabled');
+            calcularDistanciaYPrecio();
+        });
+    });
+
+    // Reset del formulario
+    document.getElementById('shipmentForm').addEventListener('reset', function() {
+        restOfForm.classList.remove('enabled');
+        vehicleImages.forEach(v => v.classList.remove('selected'));
+        tipoVehiculoInput.value = '';
+    });
+
+    // Función para guardar datos
     window.guardarDatos = function () {
         let form = document.getElementById('shipmentForm');
         if (form.checkValidity() === false) {
@@ -273,26 +209,21 @@ document.addEventListener('DOMContentLoaded', function () {
             destino: `https://www.waze.com/ul?ll=${markerDestino.getPosition().lat()},${markerDestino.getPosition().lng()}&navigate=yes`,
             tipoVehiculo: document.getElementById('tipoVehiculo').value,
             detalle: document.getElementById('detalle').value,
-            provincia: document.getElementById('provincia').value,
-            canton: document.getElementById('canton').value,
-            distrito: document.getElementById('distrito').value,
             distancia: document.getElementById('distancia').value,
             precio: document.getElementById('precio').value,
         };
 
-        enviarWhatsApp(formData);
-        enviarCorreo(formData);
-
+        // Aquí puedes agregar la lógica para enviar los datos
         Swal.fire({
             icon: 'success',
             title: '¡Datos guardados!',
             text: 'La información ha sido enviada y guardada exitosamente.'
         });
-    
+
         form.reset();
         form.classList.remove('was-validated');
     };
 
-    // Inicializar los precios por defecto
+    // Inicializar precios por defecto
     actualizarPreciosPorVehiculo();
 });
